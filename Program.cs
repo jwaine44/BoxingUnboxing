d3277d4012cf9b9bf331f2201fe54699bd8b2395
jwaine44 <jwaine44@gmail.com>
2022-07-31 10:38:59 -0400

    Initialize Repo

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿// Create an empty List of type object
+List<object> newList = new List<object>();
+
+// Add the following values to the list: 7, 28, -1, true, "chair"
+newList.Add(7);
+newList.Add(28);
+newList.Add(-1);
+newList.Add(true);
+newList.Add("chair");
+
+// Loop through the list and print all values
+foreach (var entry in newList)
+{
+    Console.WriteLine(entry);
+}
+
+// Add all values that are Int type together and output the sum
+int sum = 0;
+foreach (var entry in newList)
+{
+    if (entry is int) {
+        sum += (int)entry;
+    }
+}
+
+Console.WriteLine(sum);
